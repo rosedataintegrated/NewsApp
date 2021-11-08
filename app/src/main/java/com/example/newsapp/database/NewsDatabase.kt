1bbaea3241246f37dbbe93fb465922db47afb6c0
Benard Mathu <mathubenard@gmail.com>
2021-11-08 10:05:52 +0300

    Update gitignore to exclude platform dependent files

--- app/src/main/java/com/example/newsapp/database/NewsDatabase.kt
+++
@@ -1,9 +0,0 @@
-package com.example.newsapp.database
-
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-@Database(entities = [NewsEntity::class], version = 1, exportSchema = false)
-abstract class NewsDatabase: RoomDatabase(){
-    abstract val newsInterface: NewsDAO
-}
